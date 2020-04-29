c31d11d40402632cebec9f93620e52137e14f732
Samuel Valenzuela <Sam@Samuels-MacBook-Pro.local>
2020-04-29 02:46:20 +0200

    hola2

--- TablePartF.java
+++ TablePartF.java
@@ -31,7 +31,6 @@ public class TablePartF{
     Connection connection = ConnectionFactory.createConnection(config);
     // Instantiating HTable class
     Table table = connection.getTable(TableName.valueOf("powers"));
-    Table table1 = connection.getTable(TableName.valueOf("powers"));
 
     Scan scan = new Scan();   
 
@@ -39,35 +38,35 @@ public class TablePartF{
     scan.addColumn(Bytes.toBytes("personal"), Bytes.toBytes("power"));
     scan.addColumn(Bytes.toBytes("professional"), Bytes.toBytes("name"));
 
-    Scan scan2 = new Scan();   
-
-    scan2.addColumn(Bytes.toBytes("custom"), Bytes.toBytes("color"));
-    scan2.addColumn(Bytes.toBytes("personal"), Bytes.toBytes("power"));
-    scan2.addColumn(Bytes.toBytes("professional"), Bytes.toBytes("name"));
-
 	// DON' CHANGE THE 'System.out.println(xxx)' OUTPUT PART
 	// OR YOU WON'T RECEIVE POINTS FROM THE GRADER
 	ResultScanner scanner1 = table.getScanner(scan);
-	ResultScanner scanner2 = table1.getScanner(scan2);
+
+	List<List<String>> list = new ArrayList<List<String>>();
 
 	for (Result result1 = scanner1.next(); result1 != null; result1 = scanner1.next()){
-		for(Result result2 = scanner2.next(); result2 != null; result2 = scanner2.next()){
+		List<String> aux = new ArrayList<String>();
 
-			String c1 = new String(result1.getValue(Bytes.toBytes("custom"),Bytes.toBytes("color")));
-			String c2 = new String(result2.getValue(Bytes.toBytes("custom"),Bytes.toBytes("color")));
-			String name1 = new String(result1.getValue(Bytes.toBytes("professional"),Bytes.toBytes("name")));
-			String name2 = new String(result2.getValue(Bytes.toBytes("professional"),Bytes.toBytes("name")));
+		String col = new String(result1.getValue(Bytes.toBytes("custom"),Bytes.toBytes("color")));
+		String nam = new String(result1.getValue(Bytes.toBytes("professional"),Bytes.toBytes("name")));
+		String pow = new String(result1.getValue(Bytes.toBytes("personal"),Bytes.toBytes("power")));
 
-			if(c1.equals(c2) && !name1.equals(name2)){
-				String power1 = new String(result1.getValue(Bytes.toBytes("personal"),Bytes.toBytes("power")));
-				String power2 = new String(result2.getValue(Bytes.toBytes("personal"),Bytes.toBytes("power")));
-				System.out.println(name1 + ", " + power1 + ", " + name2 + ", " + power2 + ", "+c1);
+		aux.add(col);
+		aux.add(nam);
+		aux.add(pow);
+		list.add(aux);
+	}
+
+
+	for(int i = 0; i < list.size(); i++){
+		for(int j = i + 1; j < list.size(); j++){
+			if(list.get(i).get(0).equals(list.get(j).get(0)) && !list.get(i).get(1).equals(list.get(j).get(1))){
+				System.out.println(list.get(i).get(1) + ", " + list.get(i).get(2) + ", " + list.get(j).get(1) + ", " + list.get(j).get(2) + ", "+list.get(i).get(0));
 			}
 		}
 	}
 
 	scanner1.close();
-	scanner2.close();
 
    }
 }
